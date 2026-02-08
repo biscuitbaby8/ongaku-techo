c7f8ba91a9f783a602c0e9a1a44b354f7fbb378f
biscuitbaby8 <ss27ys@gmail.com>
2026-02-09 00:08:47 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+// index.css が無い場合のエラーを避けるため、読み込みを停止しました
+// import './index.css' 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
