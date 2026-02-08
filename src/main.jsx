0fdbdc71192b5f4aa63a54e32534d537d1051f4b
biscuitbaby8 <ss27ys@gmail.com>
2026-02-09 00:21:04 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// CSSファイルがない場合のエラーを回避
-// import './index.css' 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
