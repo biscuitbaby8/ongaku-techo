23f2112b1f0dd9a9d4f1db303ba11bd6451dcd01
biscuitbaby8 <ss27ys@gmail.com>
2026-02-09 00:03:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Tailwind CSSをここで読み込みます
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
