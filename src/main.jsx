0cacbf16ecf9525369885f43be5f3cf602d19c0a
biscuitbaby8 <ss27ys@gmail.com>
2026-02-09 00:14:14 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// index.css が無い場合のエラーを避けるため、読み込みを停止しました
+// CSSファイルがない場合のエラーを回避
 // import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
