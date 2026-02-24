727641cd79f0ede4eb795a6298dd03c30ba8621f
biscuitbaby8 <ss27ys@gmail.com>
2026-02-25 00:30:32 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: './', // ここを追加：ルート直下のファイルを静的資産として扱う
   build: {
     rollupOptions: {
       input: {
